b4fcd531d39e9fb4a5d98fcf89e500f0c788d4cc
Daniel Valcarce <davamix@gmail.com>
2011-09-02 18:39:59 +0200

    Cambios menores de reesctructuración del código

--- WPFTreeviewSample/WPFTreeviewSample/NodeItem.cs
+++ WPFTreeviewSample/WPFTreeviewSample/NodeItem.cs
@@ -2,11 +2,13 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
-using System.ComponentModel;
-using System.Collections.ObjectModel;
+
 
 namespace WPFTreeviewSample
 {
+	using System.ComponentModel;
+	using System.Collections.ObjectModel;
+
 	public class NodeItem : INotifyPropertyChanged
 	{
 		#region Miembros de INotifyPropertyChanged
